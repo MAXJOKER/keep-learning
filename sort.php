903c75cc86385dc6b97fd0e2e0a1a05de6f3dc12
MAXJOKER <callmemaxjoker@gmail.com>
2019-03-04 00:27:13 +0800

    增加希尔排序

--- sort.php
+++ sort.php
@@ -30,3 +30,55 @@ function quickSort($arr){
     array_push($left, $value);
     return array_merge($left, $right);
 }
+
+/**
+ * 堆排序
+ */
+
+/**
+ * 希尔排序
+ */
+function shellSort($arr) {
+
+  $count = count($arr);
+
+  $inc = $count;  //增量
+
+  do {
+
+    //计算增量
+
+    //$inc = floor($inc / 3) + 1;
+
+    $inc = ceil($inc / 2);
+
+    for ($i = $inc; $i < $count; $i++) {
+
+      $temp = $arr[$i];  //设置哨兵
+
+      //需将$temp插入有序增量子表
+
+      for ($j = $i - $inc; $j >= 0 && $arr[$j + $inc] < $arr[$j]; $j -= $inc) {
+
+        $arr[$j + $inc] = $arr[$j]; //记录后移
+
+      }
+
+      //插入
+
+      $arr[$j + $inc] = $temp;
+
+    }
+
+    //增量为1时停止循环
+
+  } while ($inc > 1);
+
+  return $arr;
+}
+
+$arr = [9, 2, 1, 3, 5, 7, 6, 10, 8, 666];
+//$result = quickSort($arr);
+$result = shellSort($arr);
+var_dump($result);
+//var_dump(array_reverse($result));
