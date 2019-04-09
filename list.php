5101fb1ff4f6d14f15dec0937f61c432f4c74c35
MAXJOKER <callmemaxjoker@gmail.com>
2019-04-09 21:40:58 +0800

    设计模式

--- list.php
+++ list.php
@@ -1,17 +1,30 @@
 <?php
 /**
- * 单向链表翻转
- * 2019-03-02 17:30:00
-
+ * @desc: 链表
+ * @date: 2019-03-02 17:30:00
+ * @author: junhui.zhang
+ *
+ * 链表是线性表的一种，所谓的线性表包含顺序线性表和链表，顺序线性表是用
+ * 数组实现的，在内存中有顺序排列，通过改变数组大小实现。而链表不是用顺
+ * 序实现的，用指针实现，在内存中不连续。意思就是说，链表就是将一系列不
+ * 连续的内存联系起来，将那种碎片内存进行合理的利用，解决空间的问题。
+ *
+ * 所以，链表允许插入和删除表上任意位置上的节点，但是不允许随即存取。
+ * 链表有很多种不同的类型：单向链表、双向链表及循环链表
+ *
+ * 链表本质上就是一种数据结构，主要用来动态放置数据。也可用来构建许多数
+ * 据结构，比如堆栈、队列及它们的派生。
+ *
+ *
  * 单向链表：
  *	单向链表的链表对象维护了一个 first引用，该引用指向节点链表中的第一个节点对象，
  *	每个节点对象维护一个 next 引用，next引用指向下一个节点对象；（这里注意：是引
  *	用指向的是节点对象：节点对象包含存储的数据和next引用）
  *
- *	图片地址：https://img-blog.csdn.net/20180523102549287?watermark/2/text/aHR0cH
- *	M6Ly9ibG9nLmNzZG4ubmV0L3psYl9sb3Zlcg==/font/5a6L5L2T/fontsize/400/fill/I0JBQkFCMA==/dissolve/70
+ *	图片地址：https://img-blog.csdn.net/20180523102549287?watermark/2/text/aHR0cHM6Ly9ibG9nLmNzZG4ubmV0L3psYl9sb3Zlcg==/font/5a6L5L2T/fontsize/400/fill/I0JBQkFCMA==/dissolve/70
  *	相关文章：https://blog.csdn.net/zlb_lover/article/details/80415875
  *
+ *	
  */
 
 class Node{
