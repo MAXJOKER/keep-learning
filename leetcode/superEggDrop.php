7fdcd7b97ddb61fb4a82308761f8db5ee24b4ef5
MAXJOKER <callmemaxjoker@gmail.com>
2019-05-13 10:50:26 +0800

    鸡蛋掉落问题

---
+++ leetcode/superEggDrop.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * @author junhui.zhang
+ * @date 2019-04-12 20:29
+ * @desc 鸡蛋掉落
+ */
+
+class Solution {
+
+    /**
+     * @param Integer $K
+     * @param Integer $N
+     * @return Integer
+     */
+    function superEggDrop($K, $N) {
+	if($K == 1 || $N == 1) return $N;
+
+	$i = 1;
+	$re = 0;
+	$r = 0;
+	$j = 1;
+
+	while(1){
+		$re = pow($K, $i);
+		if($re > $N) break;	
+		$i++;
+	}
+
+	$gap1 = abs(pow($K, $i - 1) - $N);
+	$gap2 = pow($K, $i - 1);
+	$mg = max($gap1, $gap2);
+
+	var_dump($mg);
+	var_dump($i);
+	if($mg > 0){
+		while(1){
+			$r = pow($K, $j);
+			if($r >= $mg) break;
+			$j++;
+		}
+	}
+	var_dump($r);
+	var_dump($j);
+
+
+	$result = $gap1 > $gap2 ? $i - 1 + $j : $i + $j;
+	
+	return $result;        
+    }
+}
+
+$obj = new Solution();
+$result = $obj->superEggDrop(3, 14);
+var_dump($result);
+
