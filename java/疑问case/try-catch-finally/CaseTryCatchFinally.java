ac7b141a81e644751fa89e2e0b5a8f689d8321e0
zhangjunhui <zhangjunhui@bigo.sg>
2021-09-03 18:02:52 +0800

    add gitignore

--- java/疑问case/try-catch-finally/CaseTryCatchFinally.java
+++ java/疑问case/try-catch-finally/CaseTryCatchFinally.java
@@ -1,3 +1,6 @@
+import java.util.List;
+import java.util.ArrayList;
+
 public class CaseTryCatchFinally {
 
     public static Integer test1() {
@@ -32,11 +35,51 @@ public class CaseTryCatchFinally {
         }
     }
 
+    public static List<String> test3() {
+        List<String> n = new ArrayList<>();
+        try {
+            n.add("try");
+            System.out.println("try, n = " + n.toString());
+            return n;
+        } catch (Exception e) {
+            n.add("catch");
+            System.out.println("catch, n = " + n.toString());
+            return n;
+        } finally {
+            n.add("finally");
+            System.out.println("finally, n = " + n.toString());
+        }
+    }
+
+    public static Integer test4() {
+        int k = 1;
+        try {
+            k++;
+            // k = k / 0;
+            System.out.println("try, k = " + k);
+            return k;
+        } catch (Exception e) {
+            k++;
+            System.out.println("catch, k = " + k);
+            return k;
+        } finally {
+            k = 6;
+            System.out.println("finally, k = " + k);
+            return k;
+        }
+    }
+
     public static void main(String[] args) {
         int i = test1();
         System.out.println("main i = " + i);
 
         int j = test2();
         System.out.println("main j = " + j);
+
+        List<String> n = test3();
+        System.out.println("main n = " + n.toString());
+
+        int k = test4();
+        System.out.println("main k = " + k);
     }
 }
