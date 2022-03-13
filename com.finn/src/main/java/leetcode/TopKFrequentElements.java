20722d72b4e490a83c957610197ff97fc9abeb85
张钧辉 <callmemaxjoker@gmail.com>
2022-03-14 00:41:40 +0800

    leetcode 前k个高频元素

--- com.finn/src/main/java/leetcode/TopKFrequentElements.java
+++ com.finn/src/main/java/leetcode/TopKFrequentElements.java
@@ -1,8 +1,15 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 
 /**
+ *
+ * @author maxjoker
+ * @date 2022-03-13 23:52
+ *
  * 347. 前 K 个高频元素
  * 给你一个整数数组 nums 和一个整数 k ，请你返回其中出现频率前 k 高的元素。你可以按 任意顺序 返回答案。
  *
@@ -30,13 +37,37 @@ import java.util.HashMap;
  * https://leetcode-cn.com/problems/top-k-frequent-elements/
  */
 public class TopKFrequentElements {
+    /**
+     * 时间复杂度：O(n + klogk)
+     * 空间复杂度：O(n + k)
+     * @param nums
+     * @param k
+     * @return
+     */
     public static int[] topKFrequent(int[] nums, int k) {
         HashMap<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
         }
 
-        return new int[]{1};
+        List<Integer> list = new ArrayList<>(map.keySet());
+        list.sort((a, b) -> map.get(b) - map.get(a));
+
+        int[] result = new int[k];
+        for (int i = 0; i < k; i++) {
+            result[i] = list.get(i);
+        }
+
+        return result;
+
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1,1,1,2,2,3};
+        int[] result = topKFrequent(nums, 2);
+        for (int i : result) {
+            System.out.println(i);
+        }
 
     }
 }
