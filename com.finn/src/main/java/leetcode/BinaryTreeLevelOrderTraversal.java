f5287c995c66e560a23b34e1a9ad269315201f5b
张钧辉 <callmemaxjoker@gmail.com>
2022-03-21 11:02:37 +0800

    leetcode 二叉树层序遍历

--- com.finn/src/main/java/leetcode/BinaryTreeLevelOrderTraversal.java
+++ com.finn/src/main/java/leetcode/BinaryTreeLevelOrderTraversal.java
@@ -4,9 +4,6 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
-import java.util.logging.Level;
-
-import static leetcode.GenerateParentheses.dfs;
 
 /**
  * @author maxjoker
