ad75adc56ed0c896abf54e7190fab02200a7fbab
张钧辉 <callmemaxjoker@gmail.com>
2022-05-01 16:20:27 +0800

    leetcode 最大正方形

---
+++ com.finn/src/main/java/leetcode/dynamicprogramming/MaximalSquare.java
@@ -0,0 +1,79 @@
+package leetcode.dynamicprogramming;
+
+/**
+ * @author maxjoker
+ * @date 2022-05-01 12:58
+ * https://leetcode-cn.com/problems/maximal-square/
+ *
+ * 221. 最大正方形
+ * 在一个由 '0' 和 '1' 组成的二维矩阵内，找到只包含 '1' 的最大正方形，并返回其面积。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：matrix = [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]
+ * 输出：4
+ * 示例 2：
+ *
+ *
+ * 输入：matrix = [["0","1"],["1","0"]]
+ * 输出：1
+ * 示例 3：
+ *
+ * 输入：matrix = [["0"]]
+ * 输出：0
+ *
+ *
+ * 提示：
+ *
+ * m == matrix.length
+ * n == matrix[i].length
+ * 1 <= m, n <= 300
+ * matrix[i][j] 为 '0' 或 '1'
+ *
+ */
+public class MaximalSquare {
+    /**
+     * 时间复杂度：O(mn)  行数与列数
+     * 空间复杂度：O(mn)
+     *
+     * 状态转移方程：dp(i,j)=min(dp(i−1,j),dp(i−1,j−1),dp(i,j−1))+1
+     * 题解：https://leetcode-cn.com/problems/maximal-square/solution/zui-da-zheng-fang-xing-by-leetcode-solution/
+     *
+     * @param matrix
+     * @return
+     */
+    public static int maximalSquare(char[][] matrix) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return 0;
+        }
+
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int[][] dp = new int[rows][cols];
+
+        int maxSide = 0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (matrix[i][j] == '1') {
+                    if (i == 0 || j == 0) {
+                        dp[i][j] = 1;
+                    } else {
+                        dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
+                    }
+
+                    maxSide = Math.max(maxSide, dp[i][j]);
+                }
+            }
+        }
+
+        return maxSide * maxSide;
+    }
+
+    public static void main(String[] args) {
+        char[][] c = new char[][]{{'1', '0', '1', '0', '0'}, {'1', '0', '1', '1', '1'}, {'1', '1', '1', '1', '1'}, {'1', '0', '0', '1', '0'}};
+        System.out.println(maximalSquare(c));
+    }
+}
