c4d08275ef395603e2b4e982c7aec717ea54489e
张钧辉 <callmemaxjoker@gmail.com>
2022-06-14 23:55:44 +0800

    java Final 关键字

--- com.finn/src/main/java/learnjava/javathread/FinalDemo.java
+++ com.finn/src/main/java/learnjava/javathread/FinalDemo.java
@@ -191,8 +191,8 @@ public class FinalDemo {
         // One More Thing
         byte b1 = 1;
         byte b2 = 2;
-        byte b3 = b1 + b2; // 报错，因为b1、b2可以自动转换成int类型的变量，运算时java虚拟机对它进行了转换，结果导致把一个int赋值给byte
-
+//        byte b3 = b1 + b2; // 报错，因为b1、b2可以自动转换成int类型的变量，运算时java虚拟机对它进行了转换，结果导致把一个int赋值给byte
+//
         final byte b5 = 1;
         final byte b6 = 2;
         byte b7 = b5 + b6; // 不会报错，因为基础类型被final修饰后其值不再改变，也就不会对b1,b2进行自动int提升，则不会出错。
