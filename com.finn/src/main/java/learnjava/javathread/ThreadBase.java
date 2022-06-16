cdc51296fca0222a273b534c8221bc09123b952c
张钧辉 <callmemaxjoker@gmail.com>
2022-06-16 22:22:19 +0800

    java 线程基础

---
+++ com.finn/src/main/java/learnjava/javathread/ThreadBase.java
@@ -0,0 +1,24 @@
+package learnjava.javathread;
+
+/**
+ * @author maxjoker
+ * @date 2022-06-16 22:14
+ *
+ * 线程基础
+ *
+ * 来源：https://pdai.tech/md/java/thread/java-thread-x-thread-basic.html
+ *
+ * question: Condition 中的 await 方法如何指定等待条件？
+ *
+ * 线程有哪几种状态? 新建 / 可运行 / 无限期等待 / 限期等待 / 阻塞 / 死亡
+ * 分别说明从一种状态到另一种状态转变有哪些方式?
+ * 通常线程有哪几种使用方式? Runnable / Callable / Thread(继承)
+ * 基础线程机制有哪些? Executor
+ * 线程的中断方式有哪些? interrupt() / Executor shutdown() shutdownNow()
+ * 线程的互斥同步方式有哪些? synchronized / ReentrantLock
+ * 如何比较和选择?
+ * 线程之间有哪些协作方式? join() / wait(), notify(), notifyAll() / await(), signal(), signalAll()
+ *
+ */
+public class ThreadBase {
+}
