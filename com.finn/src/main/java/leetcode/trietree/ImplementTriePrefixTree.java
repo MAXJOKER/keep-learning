be270d70ae4c0d17571bd94ecf1f75a5668c7aa0
张钧辉 <callmemaxjoker@gmail.com>
2022-09-01 01:07:25 +0800

    leetcode 208. 实现 Trie (前缀树)

---
+++ com.finn/src/main/java/leetcode/trietree/ImplementTriePrefixTree.java
@@ -0,0 +1,102 @@
+package leetcode.trietree;
+
+/**
+ * @author maxjoker
+ * @date 2022-09-01 00:49
+ *
+ * https://leetcode.cn/problems/implement-trie-prefix-tree/
+ *
+ * 208. 实现 Trie (前缀树)
+ * Trie（发音类似 "try"）或者说 前缀树 是一种树形数据结构，用于高效地存储和检索字符串数据集中的键。这一数据结构有相当多的应用情景，例如自动补完和拼写检查。
+ *
+ * 请你实现 Trie 类：
+ *
+ * Trie() 初始化前缀树对象。
+ * void insert(String word) 向前缀树中插入字符串 word 。
+ * boolean search(String word) 如果字符串 word 在前缀树中，返回 true（即，在检索之前已经插入）；否则，返回 false 。
+ * boolean startsWith(String prefix) 如果之前已经插入的字符串 word 的前缀之一为 prefix ，返回 true ；否则，返回 false 。
+ *
+ *
+ * 示例：
+ *
+ * 输入
+ * ["Trie", "insert", "search", "search", "startsWith", "insert", "search"]
+ * [[], ["apple"], ["apple"], ["app"], ["app"], ["app"], ["app"]]
+ * 输出
+ * [null, null, true, false, true, null, true]
+ *
+ * 解释
+ * Trie trie = new Trie();
+ * trie.insert("apple");
+ * trie.search("apple");   // 返回 True
+ * trie.search("app");     // 返回 False
+ * trie.startsWith("app"); // 返回 True
+ * trie.insert("app");
+ * trie.search("app");     // 返回 True
+ *
+ *
+ * 提示：
+ *
+ * 1 <= word.length, prefix.length <= 2000
+ * word 和 prefix 仅由小写英文字母组成
+ * insert、search 和 startsWith 调用次数 总计 不超过 3 * 10^4 次
+ *
+ */
+public class ImplementTriePrefixTree {
+
+    public static void main(String[] args) {
+        Trie t = new Trie();
+        t.insert("apple");
+        t.search("apple");
+    }
+}
+
+/**
+ * 时间复杂度：初始化为 O(1)，其余操作为 O(|S|)，其中 |S|是每次插入或查询的字符串的长度。
+ * 空间复杂度：O(∣T∣⋅Σ)，其中 |T| 为所有插入字符串的长度之和，Σ 为字符集的大小，本题 Σ=26。
+ */
+class Trie {
+    private Trie[] children;
+    private Boolean isEnd;
+
+    public Trie() {
+        children = new Trie[26];
+        isEnd = false;
+    }
+
+    public void insert(String word) {
+        Trie node = this;
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+            int index = ch - 'a';
+            if (node.children[index] == null) {
+                node.children[index] = new Trie();
+            }
+            node = node.children[index];
+        }
+        node.isEnd = true;
+    }
+
+    public boolean search(String word) {
+        Trie node = searchPrefix(word);
+        return node != null && node.isEnd;
+    }
+
+    public boolean startsWith(String prefix) {
+        return searchPrefix(prefix) != null;
+    }
+
+    private Trie searchPrefix(String word) {
+        Trie node = this;
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+            int index = ch - 'a';
+            if (node.children[index] == null) {
+                return null;
+            }
+            node = node.children[index];
+        }
+
+        return node;
+    }
+}
